354e6600c884e745e9390b6cc0c8f61a9a1ade88
Aseel Al Dallal <aseelaldallal@gmail.com>
2018-10-03 21:44:01 -0400

    use custom pipe in template

--- src/app/shorten.pipe.ts
+++ src/app/shorten.pipe.ts
@@ -8,7 +8,10 @@ import { PipeTransform, Pipe } from "@angular/core";
 export class ShortenPipe implements PipeTransform {
 
      transform(value: any) {
-          return value.substr(0, 10);
+          if (value.length > 10 ) {
+               return value.substr(0, 10) + "...";
+          }
+          return value;
      }
 
 }
