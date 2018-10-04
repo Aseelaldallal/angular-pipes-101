23df9b657e511e533e68a48652cc768f6d7571da
Aseel Al Dallal <aseelaldallal@gmail.com>
2018-10-03 21:40:30 -0400

    create custom pipe and add it to declarations

--- src/app/shorten.pipe.ts
+++ src/app/shorten.pipe.ts
@@ -1,5 +1,10 @@
-export class ShortenPipe {
+import { PipeTransform } from "@angular/core";
 
+// implements PipeTransform forces you to implement transform
+export class ShortenPipe implements PipeTransform {
 
+     transform(value: any) {
+          return value.substr(0, 10);
+     }
 
 }
