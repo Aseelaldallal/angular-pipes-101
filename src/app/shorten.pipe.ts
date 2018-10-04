d0ff36b8f19bb16ef69039f2484afea2f534e96f
Aseel Al Dallal <aseelaldallal@gmail.com>
2018-10-03 21:45:48 -0400

    pass parameter into pipe

--- src/app/shorten.pipe.ts
+++ src/app/shorten.pipe.ts
@@ -7,9 +7,9 @@ import { PipeTransform, Pipe } from "@angular/core";
 })
 export class ShortenPipe implements PipeTransform {
 
-     transform(value: any) {
-          if (value.length > 10 ) {
-               return value.substr(0, 10) + "...";
+     transform(value: any, limit: number) {
+          if (value.length > limit ) {
+               return value.substr(0, limit) + "...";
           }
           return value;
      }
